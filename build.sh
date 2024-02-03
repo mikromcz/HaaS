992f014aaa2b946bbc050f7102f9a9291f0033da
Roman Mikulka <mikrom@mikrom.cz>
2024-02-03 18:31:16 +0100

    added comments

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+# pull alpine
+echo "Pull Alpine Python"
+#docker pull python:alpine
+docker pull python:3.12-alpine
+
+# build haas
+echo "Build HaaS with current date label"
 #docker build -t haas .
 docker build -t mikromcz/haas --label "org.opencontainers.image.version=2.0.2" --label "org.opencontainers.image.created=$(date +'%F')" .
